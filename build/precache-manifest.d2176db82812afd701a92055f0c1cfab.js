064b6a55bcf3338341bdd621b5c33fdf5ab3f911
Drarig29 <corentingirard.dev@gmail.com>
2020-04-07 13:36:44 +0200

    Add build folder

---
+++ build/precache-manifest.d2176db82812afd701a92055f0c1cfab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0fa6f0509dfab4370c3253c5061a9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67dc8fed84cb039637f5",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e101a357b592550c213c",
+    "url": "/static/css/main.205cf813.chunk.css"
+  },
+  {
+    "revision": "67dc8fed84cb039637f5",
+    "url": "/static/js/2.3dcd5ddc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3dcd5ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e101a357b592550c213c",
+    "url": "/static/js/main.a2927c5b.chunk.js"
+  },
+  {
+    "revision": "31f7d2674e2b3e440fbb",
+    "url": "/static/js/runtime-main.4d33088d.js"
+  }
+]);
