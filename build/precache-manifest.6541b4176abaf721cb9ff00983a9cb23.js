cf9794041521a9a344350a384c0ac1d0a42f7cc9
Drarig29 <corentingirard.dev@gmail.com>
2020-04-07 17:18:02 +0200

    Build

---
+++ build/precache-manifest.6541b4176abaf721cb9ff00983a9cb23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2799aa5878d632338c01081286134f0d",
+    "url": "/draw-on-it/index.html"
+  },
+  {
+    "revision": "67dc8fed84cb039637f5",
+    "url": "/draw-on-it/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a9ea2e9fa21753661e44",
+    "url": "/draw-on-it/static/css/main.205cf813.chunk.css"
+  },
+  {
+    "revision": "67dc8fed84cb039637f5",
+    "url": "/draw-on-it/static/js/2.3dcd5ddc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/draw-on-it/static/js/2.3dcd5ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ea2e9fa21753661e44",
+    "url": "/draw-on-it/static/js/main.32cb3775.chunk.js"
+  },
+  {
+    "revision": "ce9bdc06587404d1dbb8",
+    "url": "/draw-on-it/static/js/runtime-main.48f461f5.js"
+  }
+]);
