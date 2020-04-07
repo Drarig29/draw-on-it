782ec66ec1b84a084465996b3260ce33893e5701
Drarig29 <corentingirard.dev@gmail.com>
2020-04-07 12:05:08 +0200

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import CanvasDraw from "react-canvas-draw";
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
-function App() {
-    return (
-        <div className="App">
-            <CanvasDraw className="canvas" canvasWidth="60%" hideGrid="true" />
-        </div>
-    );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { imgSrc: "", canvasHeight: "400px" };
+        this.canvas = createRef();
+    }
+
+    componentDidMount() {
+        let img = new Image();
+        img.src = "https://i.imgur.com/oKYIL7y.jpg";
+        img.onload = () => {
+            const currentWidth = this.canvas.current.canvasContainer.clientWidth;
+            let height = (img.height / img.width) * currentWidth;
+            this.setState({ imgSrc: img.src, canvasHeight: `${height}px` });
+        };
+    }
+
+    componentDidUpdate() {
+        this.canvas.current.drawImage();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Button>Hello</Button>
+                <CanvasDraw className="canvas" ref={this.canvas} imgSrc={this.state.imgSrc} canvasWidth="70%" canvasHeight={this.state.canvasHeight}
+                    hideGrid={true} />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
